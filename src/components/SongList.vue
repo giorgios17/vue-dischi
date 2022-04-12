ab8ba76c06f6813a6cf26c2c0ee1ef1dc7bb7597
giorgios17 <giosur@live.it>
2022-04-12 09:26:38 +0200

    removed html comment

--- src/components/SongList.vue
+++ src/components/SongList.vue
@@ -2,10 +2,6 @@
   <main>
     <select @change="selectedLabel($event)">
       <option value="">All</option>
-      <!-- <option value="Rock">Rock</option>
-      <option value="Pop">Pop</option>
-      <option value="Jazz">Jazz</option>
-      <option value="Metal">Metal</option> -->
       <option v-for="genere in uniqueGenre()" :key="genere" :value="genere">
         {{ genere }}
       </option>
